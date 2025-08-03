c7c96427670a0e974002247ce7d4a33a72843b00
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-08-03 08:28:49 +0530

    Create MergeSort.java
    megre sort by using recursion.

---
+++ recursion/MergeSort.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 7, 8, 3, 9, 10};
+        mergesort(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void mergesort(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+        int mid = (s + e) / 2;
+        mergesort(arr, s, mid);
+        mergesort(arr, mid, e);
+        merge(arr, s, mid, e);
+    }
+
+    public static void merge(int[] arr, int s, int mid, int e) {
+        int[] mix = new int[e - s];
+        int i = s;
+        int j = mid;
+        int k = 0;
+
+        while (i < mid && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k++] = arr[i++];
+            } else {
+                mix[k++] = arr[j++];
+            }
+        }
+        while (i < mid) {
+            mix[k++] = arr[i++];
+        }
+        while (j < e) {
+            mix[k++] = arr[j++];
+        }
+        for (int l = 0; l < mix.length; l++) {
+            arr[s + l] = mix[l];
+
+        }
+    }
+}
