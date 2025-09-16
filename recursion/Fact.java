9020ef5e3a1f4bb55a8e3caff23b6169ab306789
Pranav Wadkar <pranavwadkar2335@gmail.com>
2025-09-16 22:27:40 +0530

    Update Fact.java
    add comment

--- recursion/Fact.java
+++ recursion/Fact.java
@@ -18,7 +18,7 @@ public class Fact {
     }
 
     public static int fact(int n) {
-        if (n==0 && n==1){
+        if (n==0 && n==1){ //best case
             return 1;
         }
         if(n==2) {
